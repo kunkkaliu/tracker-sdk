4453cbeb8ba166b861eaf262f606371943e9518b
kunkkaliu <zudapang@qq.com>
2021-01-27 17:52:46 +0800

    fix: 优化babel相关plugin的配置

--- .babelrc.js
+++ .babelrc.js
@@ -1,15 +1,18 @@
 module.exports = {
   "presets": [["@babel/preset-env", {
+    "modules": false,
     "useBuiltIns": false,
     "targets": {
       "browsers": [
         "> 1%",
         "last 2 versions",
-        "ie >= 9",
+        "ie >= 9"
       ]
     }
   }]],
-  "plugins": ["@babel/plugin-transform-runtime", "@babel/plugin-proposal-class-properties"],
+  "plugins": [["@babel/plugin-transform-runtime", {
+    "corejs": 3
+  }], "@babel/plugin-proposal-class-properties"],
   "env": {
     "cjs": {
       "plugins": ["@babel/plugin-transform-modules-commonjs"]
@@ -17,9 +20,18 @@ module.exports = {
     "es": {
       "presets": [["@babel/preset-env", {
         "modules": false,
+        "useBuiltIns": false,
+        "targets": {
+          "browsers": [
+            "> 1%",
+            "last 2 versions",
+            "ie >= 9"
+          ]
+        }
       }]],
       "plugins": [["@babel/plugin-transform-runtime", {
         useESModules: true,
+        "corejs": 3
       }]]
     }
   }
